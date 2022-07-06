76570306494ddbd12aff2c65aad1b1841c7faecd
tanya <grace.08de98@gmail.com>
2022-07-06 16:16:07 +0900

    change anything

--- keysite/models.py
+++ keysite/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from . import tuple
 
-# Create your models here.
+class Post(models.Model):
+    program = models.CharField(max_length=200)
+    cmd_category = models.CharField(choices=tuple.category_item, max_length=200)
+    cmd_name = models.CharField(max_length=200)
+    cmd_description = models.CharField(max_length=200)
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
+
---
+++ keysite/tuple.py
@@ -0,0 +1,24 @@
+
+category_item =(
+    ('Feaquently_Used_Key','Feaquently_Used_Key'),
+    ('Ribbon','Ribbon'),
+    ('Navigate','Navigate'),
+    ('Preview','Preview'),
+    ('Select','Select'),
+    ('Extend_Selection','Extend_Selection'),
+    ('Edit','Edit'),
+    ('Align','Align'),
+    ('Format','Format'),
+    ('Insert', 'Insert'),
+    ('Web','Web'),
+    ('Table','Table'),
+    ('Review','Review'),
+    ('Reference','Reference'),
+    ('Mail','Mail'),
+    ('Field', 'Field'),
+    ('Language', 'Language'),
+    ('View', 'View'),
+    ('Outline', 'Outline'),
+    ('Fn_key', 'Fn_key'),
+)
+
