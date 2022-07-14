eeeeb3c2f988f909cab493d24d87b7a9807ea7da
tanya <grace.08de98@gmail.com>
2022-07-14 19:12:34 +0900

    connect HTML, Views.py, urls.py

--- keysite/views.py
+++ keysite/views.py
@@ -5,18 +5,19 @@ def home(request):
     return render(request, '../templates/home.html')
 
 def Win_HWP_key_list(request):
-    cmd_list = Cmd.objects.all()
+    cmd_list = Cmd.objects.filter(choice_OS='Windows', program='HWP')
     return render(request, '../templates/Win_HWP_key_list.html',{'cmd_list':cmd_list})
 
 def Win_Word_key_list(request):
-    cmd_list = Cmd.objects.all()
+    cmd_list = Cmd.objects.filter(choice_OS='Windows', program='Word')
     return render(request, '../templates/Win_Word_key_list.html',{'cmd_list':cmd_list})
 
 def MacOS_HWP_key_list(request):
-    cmd_list = Cmd.objects.all()
+    cmd_list = Cmd.objects.filter(choice_OS='MacOS', program='HWP')
     return render(request, '../templates/MacOS_HWP_key_list.html',{'cmd_list':cmd_list})
 
 def MacOS_Word_key_list(request):
-    cmd_list = Cmd.objects.all()
+    cmd_list = Cmd.objects.filter(choice_OS='MacOS', program='Word')
     return render(request, '../templates/MacOS_Word_key_list.html',{'cmd_list':cmd_list})
+
 
